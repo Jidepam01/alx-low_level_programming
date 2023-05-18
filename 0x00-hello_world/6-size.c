3fc5824ea878e8e8b6f68a6a0b1a045c187ae654
jidepam01 <jidepam01@gmail.com>
2023-05-18 16:50:29 -0700

    Print size of diff data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,9 +14,9 @@ int main(void)
 	long long int y;
 	float z;
 
-printf("Size of a char: %lu\n", sizeof(char));
-printf("Size of an int: %lu\n", sizeof(int));
-primtf("Size of a long int: %lu\n", sizeof(long int));
-printf("Size of a long long int: %lu\n", sizeof(long long int));
-printf("Size of a float: %lu\n", sizeof(float));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(v));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(w));
+primtf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(x));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(y));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(z));
 return (0);
