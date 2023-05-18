91946b9e71cb2652147d05ae7ace39eeb14f1cae
jidepam01 <jidepam01@gmail.com>
2023-05-18 09:34:09 -0700

    Print texts using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints out texts using puts
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
