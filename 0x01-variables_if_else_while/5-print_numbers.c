b870245e46a031fa8ac9c098119eb4d30bbdae30
jidepam01 <jidepam01@gmail.com>
2023-05-19 15:04:27 -0700

    Print base10 single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ * main - Prints digit 0 through to 9
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
