b22597325e3920eb186e34e7aa7a91aa56f7b881
jidepam01 <jidepam01@gmail.com>
2023-05-19 13:31:49 -0700

    Print alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alph[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
