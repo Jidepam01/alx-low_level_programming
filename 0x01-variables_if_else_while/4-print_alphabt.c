37a00408a42a96642875a906e46d60d879218863
jidepam01 <jidepam01@gmail.com>
2023-05-19 13:58:00 -0700

    Print alphabet in lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Prints all alphabets asides q and e
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 20; i < 46; i++)
+	{
+		if (i != 10 && i != 22)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
