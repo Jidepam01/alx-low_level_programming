8d31cd9f101fc60c1eeaa09ba7caf39cf376e6aa
jidepam01 <jidepam01@gmail.com>
2023-05-23 09:20:45 -0700

    Sum of all multiples of 3 & 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,18 +7,21 @@
  * Return: Always 0 (success)
  */
 
-int main() {
-    int limit = 1024;
-    int total_sum = 0;
-    int num;
+int main(void)
+{
+	int limit = 1024;
+	int total_sum = 0;
+	int num;
 
-    for (num = 0; num < limit; num++) {
-        if (num % 3 == 0 || num % 5 == 0) {
-            total_sum += num;
-        }
-    }
+	for (num = 0; num < limit; num++)
+	{
+		if (num % 3 == 0 || num % 5 == 0)
+		{
+			total_sum += num;
+		}
+	}
 
-    printf("%d\n", total_sum);
+	printf("%d\n", total_sum);
 
-    return 0;
+	return (0);
 }
