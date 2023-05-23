19e8c60fc9a0cd45eb1968bcc2cda0a0fdc37b32
jidepam01 <jidepam01@gmail.com>
2023-05-23 09:09:38 -0700

    sum of multiples 0f 3 0r 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Lists all the natural numbers below 1024 (excluded),
+ * that are multiples of 3 or 5.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main() {
+    int limit = 1024;
+    int total_sum = 0;
+    int num;
+
+    for (num = 0; num < limit; num++) {
+        if (num % 3 == 0 || num % 5 == 0) {
+            total_sum += num;
+        }
+    }
+
+    printf("%d\n", total_sum);
+
+    return 0;
+}
