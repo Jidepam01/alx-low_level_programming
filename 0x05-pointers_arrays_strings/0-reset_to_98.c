6be006a2d806e75bc879cd082d5a8bf2ab72b13f
jidepam01 <jidepam01@gmail.com>
2023-05-29 19:08:15 -0700

    Dereferencing an int value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * main - function that takes a pointer to an int
- * as parameter and updates the value it points 
+ * reset_to_98 - function that takes a pointer to an int
+ * as parameter and updates the value it points
  * to to 98.
  * @n: int to check
- *
- * Return: Always 0 (success)
+ * Return: void
  */
 
 void reset_to_98(int *n)
