205f16a036b70f7638c84b1fa566fa05616564a2
jidepam01 <jidepam01@gmail.com>
2023-05-29 18:50:27 -0700

    Derefrencing an integer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - function that takes a pointer to an int
+ * as parameter and updates the value it points 
+ * to to 98.
+ * @n: int to check
+ *
+ * Return: Always 0 (success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
