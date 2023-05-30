fb2c1646a140c654ad8a98fe5abdc58204858039
jidepam01 <jidepam01@gmail.com>
2023-05-29 22:04:57 -0700

    Return string length

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+/**
+ * print_rev - function that prints a string in reverse
+ * @s: pointer to the string to be printed in reverse
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (s[i] == '\0')
+			break;
+		i++;
+	}
+
+	for (i--; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
