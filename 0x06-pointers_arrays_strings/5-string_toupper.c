d9404a9a4a2324749855d4c065a0b80cba34a0e9
jidepam01 <jidepam01@gmail.com>
2023-06-01 02:59:38 -0700

    Change all lowercase letters to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+/**
+ * string_toupper - a function that changes all lowercase to uppercase letters
+ * @str: string letter
+ * Return: pointer to uppercase
+ */
+
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'A')
+		{
+			*ptr = *ptr - ('a' - 'A');
+		}
+		ptr++;
+	}
+
+	return (str);
+}
