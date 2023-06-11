2c08dbcc9ca025ec129494f36ab94a2dce690fc3
jidepam01 <jidepam01@gmail.com>
2023-06-11 10:07:14 -0700

    Program prints num of arguments passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,7 @@
 /**
  * main - program prints number of arguments passed into it
  * @argc: numbers of arguments passed to command-line
- * @argv: arrays of strings of char for arguments passed
+ * @argv: arrays of strings of char for arguments passed to CLI
  * Return: always 0 (success)
  */
 
@@ -12,5 +12,6 @@ int main(int argc, char *argv[])
 {
 	(void)argv;
 	printf("%d\n", argc - 1);
+
 	return (0);
 }
