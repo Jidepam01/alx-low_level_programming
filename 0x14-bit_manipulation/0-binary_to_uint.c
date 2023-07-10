b67664534186495c8cf95ade29cf491c65d2bcdc
jidepam01 <jidepam01@gmail.com>
2023-07-10 09:20:34 -0700

    Func converts binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,31 +4,25 @@
   * @b: binary number
   * Return: the converted number or 0 if it fails
   */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int uint, place;
 	int i;
+	unsigned int j;
 
+	j = 0;
 	if (!b)
 		return (0);
-
-	for (i = 0; b[i]; i++)
-		;
-	i	--;
-	for (place = 1, uint = 0; i >= 0; --)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] == '0')
-		{
-			place *= 2;
-			continue;
-		}
-		else if (b[i] == '1')
-		{
-			uint += place;
-			place *= 2;
-			continue;
-		}
-		return (0);
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 	}
-	return (uint);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
+	}
+	return (j);
 }
